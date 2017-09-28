46768ec9da7e394b3e8c5ea7dc4d135c3b238bb7
pilarccasi <pilarcc88@gmail.com>
2017-09-28 18:37:18 +0200

    ejercicio 11 50%

---
+++ ex/11_HTML5VideoPlayer/scripts.js
@@ -0,0 +1,47 @@
+// Get our elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+
+//functions
+function togglePlay() {
+    if(video.paused) {
+        video.play();
+    }  else {
+        video.pause();
+    }
+
+}
+
+function updateButton() {
+ const icon = this.paused ? '►' : '❚ ❚';
+ toggle.textContent = icon;
+}
+
+function skip() {
+    console.log(this.dataset);
+    video.currentTime += parseFloat(this.dataset.skip)
+}
+
+function handleRangeUpdate() {
+    video['this.name'] = this.value;
+    console.log(this.value);
+    console.log(this.name);
+}
+
+function handleProgress() 
+
+/* Hook up the event listners */
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+ranges.forEach(range => range.addEventListener('mousemove', handleRangeUpdate));
